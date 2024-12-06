1271a572932baa13ff9400d4bed5e98dec019d17
Gleb Ostrowski <98348049+rd-gleb-ostrowski@users.noreply.github.com>
2024-12-06 09:43:26 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/bin/06.rs

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,7 +1,71 @@
+use std::str::FromStr;
+
 advent_of_code::solution!(6);
 
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl FromStr for Direction {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "^" => Ok(Direction::Up),
+            ">" => Ok(Direction::Right),
+            "<" => Ok(Direction::Left),
+            "v" => Ok(Direction::Down),
+            _ => Err(()),
+        }
+    }
+}
+
+struct Guard {
+    row: usize,
+    column: usize,
+    direction: Direction,
+}
+
+impl Guard {
+    fn new(row: usize, column: usize) -> Self {
+        Self {
+            row,
+            column,
+            direction: Direction::Up,
+        }
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
+    let input = input.lines().collect::<Vec<_>>();
+    let mut guard_postion = None;
+
+    for (row, l) in input.iter().enumerate() {
+        for column in 0..l.len() {
+            let s = dbg!(&l[column..column + 1]);
+            let d = Direction::from_str(s);
+            if let Ok(direction) = d {
+                guard_postion = Some(Guard {
+                    row,
+                    column,
+                    direction,
+                });
+                break;
+            }
+        }
+    }
+
     None
+
+    /*
+    (0,0)  (0,column)
+    (1,0)   (1,column)
+    ....
+    (row,0).....(row,column)
+     */
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
